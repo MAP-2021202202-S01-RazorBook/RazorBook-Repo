246d488c27c1edf17043eb3fbbaec1c435c6908e
Hamzeh Bajbouj <hamzehbouj@gmail.com>
2022-05-25 13:29:40 +0300

    new_config

---
+++ lib/models/customerUserSide/customer_registration_model.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class CustomerDatabaseManager {
+
+final CollectionReference customerRegisteree = FirebaseFirestore.instance.collection('customers');
+
+Future<void> addNewCustomer(String name, String age, String gender,
+String contact, String email, String password) async{
+
+   await customerRegisteree.add({
+    'name': name,
+    'age': age,
+    'gender': gender,
+    'contact': contact,
+    'email': email,
+    'isCustomer': true
+  }).then((value) => null).onError((error, stackTrace){print("error ${error.toString()}");});
+
+  FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password)
+  .then((value) => null).onError((error, stackTrace){print("error ${error.toString()}");});
+}
+}
---
+++ lib/views/login_view.dart
@@ -0,0 +1,118 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import '../view_model/login_view_model.dart';
+import '../helpers/helper_widgets.dart';
+import '../views/signup_view.dart';
+import '../views/forgot_password_view.dart';
+import '../views/home_view.dart';
+import 'barbershop_profile_view.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(0, 219, 201, 201),
+          Color.fromARGB(255, 21, 141, 91),
+          Color(0x610BE803)
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.1, 20, 15),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/logo.png"),
+                SizedBox(
+                  height: 20,
+                ),
+                inputField("Enter your Email", Icons.mail_outline, false,
+                    emailController),
+                SizedBox(
+                  height: 20,
+                ),
+                inputField("Enter your Password", Icons.lock_outline, true,
+                    passwordController),
+                SizedBox(
+                  height: 30,
+                ),
+                confirmButton(context, "Login", () async {
+                  try {
+                    await loginAttempt(
+                        emailController.text, passwordController.text);
+                    // FORWARD TO PAGE HERE:
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) => const BarberShopProfileView()),
+                    );
+                  } catch (e) {
+                    String errorMessage = e.toString();
+                    // setState(() {});
+                    var snackBar = messageSnackBar(errorMessage, Colors.red);
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                }),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ForgotPasswordView()));
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        // horizontal: 20,
+                        vertical:
+                            15), //apply padding horizontal or vertical only
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+                signup(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row signup() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account?",
+            style: TextStyle(color: Colors.white)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => TypeView()));
+          },
+          child: const Text(
+            " Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+}
