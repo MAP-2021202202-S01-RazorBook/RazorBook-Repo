0ff64eecb775523a6a358e97f593a5fb714688db
Mohamed Alganzory <vidyatuts@gmail.com>
2022-05-29 19:36:59 +0800

    implement forget password view and view model

--- lib/view_model/forgot_password_view_model.dart
+++ lib/view_model/forgot_password_view_model.dart
@@ -1,5 +1,18 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import '../app/service_locator/service_locator.dart';
+import 'base_view_model.dart';
 
-Future sendResetEmail(String email) async {
-  await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+class ForgotPasswordViewModel extends BaseModel {
+  final AuthenticationService _authenticationService =
+      locator<AuthenticationService>();
+
+  Future<void> forgotPassword(String email) async {
+    setBusy(true);
+    try {
+      await _authenticationService.recoverPassword(email: email);
+    } catch (e) {
+      rethrow;
+    } finally {
+      setBusy(false);
+    }
+  }
 }
--- lib/views/forgot_password_view.dart
+++ lib/views/forgot_password_view.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../app/service_locator/service_locator.dart';
 import '../views/login_view.dart';
 import '../helpers/helper_widgets.dart';
 import '../view_model/forgot_password_view_model.dart';
@@ -16,66 +18,76 @@ class _ForgotPasswordViewState extends State<ForgotPasswordView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Color.fromARGB(0, 219, 201, 201),
-          Color.fromARGB(255, 21, 141, 91),
-          Color(0x610BE803)
-        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(
-                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
-            child: Column(
-              children: <Widget>[
-                logoWidget("assets/images/logo.png"),
-                const SizedBox(
-                  height: 20,
+    return ChangeNotifierProvider<ForgotPasswordViewModel>(
+      create: (_) => locator<ForgotPasswordViewModel>(),
+      child: Consumer<ForgotPasswordViewModel>(
+        builder: (context, model, child) => Scaffold(
+          body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Color.fromARGB(0, 219, 201, 201),
+              Color.fromARGB(255, 21, 141, 91),
+              Color(0x610BE803)
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(
+                    20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+                child: Column(
+                  children: <Widget>[
+                    logoWidget("assets/images/logo.png"),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    inputField("Enter your Email", Icons.mail_outline, false,
+                        emailController),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    confirmButton(
+                      context,
+                      "Submit",
+                      () async {
+                        try {
+                          await model.forgotPassword(emailController.text);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginView()));
+                          const snackBar = SnackBar(
+                            backgroundColor: Colors.green,
+                            content: Text(' Email sent successfully'),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        } catch (e) {
+                          String errorMessage = e.toString();
+                          // setState(() {});
+                          var snackBar =
+                              messageSnackBar(errorMessage, Colors.red);
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }
+                      },
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginView()));
+                      },
+                      child: const Text(
+                        "Back to Login",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ],
                 ),
-                inputField("Enter your Email", Icons.mail_outline, false,
-                    emailController),
-                const SizedBox(
-                  height: 30,
-                ),
-                confirmButton(
-                  context,
-                  "Submit",
-                  () async {
-                    try {
-                      await sendResetEmail(emailController.text);
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => LoginView()));
-                      const snackBar = SnackBar(
-                        backgroundColor: Colors.green,
-                        content: Text(' Email sent successfully'),
-                      );
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    } catch (e) {
-                      String errorMessage = e.toString();
-                      // setState(() {});
-                      var snackBar = messageSnackBar(errorMessage, Colors.red);
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    }
-                  },
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => LoginView()));
-                  },
-                  child: const Text(
-                    "Back to Login",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ),
