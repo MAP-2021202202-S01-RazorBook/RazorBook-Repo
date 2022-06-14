607d9d4b2bd5603428c0f8f341fbce858f7b01c4
Amit Hasan Sadhin <samithasan7866@gmail.com>
2022-06-14 15:38:01 +0800

    Search Operation created

--- lib/views/common_widgets/search_bar.dart
+++ lib/views/common_widgets/search_bar.dart
@@ -1,18 +1,54 @@
-import 'package:flutter/material.dart';
-import 'package:razor_book/helpers/assets.dart';
-import 'package:razor_book/helpers/colors.dart';
+import 'dart:developer';
 
-class SearchBar extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:razor_book/helpers/colors.dart';
+import 'package:razor_book/models/user.dart';
+import 'package:razor_book/view_model/barber_profile_view_model.dart';
+import 'package:razor_book/views/customer_screens/chome_page/shop_search_result_page.dart';
+
+class SearchBar extends StatefulWidget {
   const SearchBar({Key? key}) : super(key: key);
 
   @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _searchController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    BarberProfileViewModel model = context.watch<BarberProfileViewModel>();
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 32),
       child: SizedBox(
         height: 48,
         child: TextFormField(
+          onFieldSubmitted: (v) {
+            print(v);
+
+            if (v.isNotEmpty) {
+              List<User>? shopList = model.searchShopList(v);
+              log("shopList ==> $shopList");
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ShopSearchResult(
+                    shopList: shopList!,
+                  ),
+                ),
+              );
+            }
+          },
           textAlignVertical: TextAlignVertical.center,
+          controller: _searchController,
           decoration: InputDecoration(
             hintText: "Search Barbar",
             hintStyle:
